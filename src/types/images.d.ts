21e98f8b204a52a0ba0873009eb6a8b21844b711
Martin <palmartin99@gmail.com>
2025-04-24 23:09:18 +0200

    Image dt

---
+++ src/types/images.d.ts
@@ -0,0 +1,14 @@
+declare module '*.jpg' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.jpeg' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.JPG' {
+    const content: string;
+    export default content;
+} 
